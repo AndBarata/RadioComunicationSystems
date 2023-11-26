4c8e04c395bf207419d25f77eacb4ec4c7ea4b5e
AndBarata <146003937+AndBarata@users.noreply.github.com>
2023-11-26 21:39:34 +0000

    Update of lab1

--- Lab1/analyse_data.m
+++ Lab1/analyse_data.m
@@ -9,11 +9,11 @@ fprintf("\nStart of the program\n");
 
 load('data/MatLab_20231102.mat');  % Load the .mat file
 load('data/MatLab_20231122.mat'); 
-sinal = [sinal; sinal2];
-ruido = [ruido; ruido2];
+%sinal = [sinal; sinal2];
+%ruido = [ruido; ruido2];
 
 % Plot the full date 
-figure(1); plot(sinal.Date, sinal.Val, '.-', ruido.Date, ruido.Val, '.-'); title('Satalite Signal'); grid on;
+figure(1); plot(sinal.Date, sinal.Val, '.-', ruido.Date, ruido.Val, '.-'); title('Satalite Signal'); grid on; legend("signal", "noise");
 
 t_lost_signal = []; % Mark the timestamp where signal was loss
 
@@ -41,16 +41,18 @@ end
 fprintf(" - Number of lostes of signal: %d\n", count);
 
 
+load('data/temp_and_rain_06_22_nov.mat');  % Load the .mat file
 load('data/temp_and_rain_data.mat');  % Load the .mat file
 
 snr = sinal.Val - ruido.Val;
+%temp_and_rain = [temp_and_rain; rain_temp_06_22_nov];
 
 snr_rain_figure = figure;
 sp(1) = subplot(1,1,1); plot(sinal.Date, sinal.Val, '.-', ruido.Date, ruido.Val, '.-'); title('Signal vs Noise: blue -> signal | red -> noise'); xlabel("date"); ylabel("Magnitude (dB"); grid on;
-sp(2) = subplot(1,1,1); plot(sinal.Date, sinal.Val/abs(max(sinal.Val))+2, '.-', temp_and_rain.datetime, temp_and_rain.precip/max(temp_and_rain.precip), '.-'); title('Percipitation Data: blue -> signal | red -> precipitation'); xlabel("date"); ylabel("Magnitude Normalized"); grid on;
+sp(2) = subplot(1,1,1); plot(sinal.Date, sinal.Val/abs(max(sinal.Val))+2, '.-', temp_and_rain.datetime, temp_and_rain.precip/max(temp_and_rain.precip), '.-'); title('Percipitation Data: blue -> signal | red -> precipitation'); xlabel("date"); ylabel("Magnitude Normalized"); grid on; 
 sp(3) = subplot(1,1,1); plot(sinal.Date, snr/max(snr), '-', temp_and_rain.datetime, temp_and_rain.precip/max(temp_and_rain.precip), '.-'); title('Rain vs SNR: blue -> rain | red -> snr | yellow -> signal loss'); grid on;
 %yline(6, '-');
-step = 0.1; % 
+step = 0.05; % 
 ignore_counter = 0;
 for i = 1: length(t_lost_signal)
         xregion(sp(1), t_lost_signal(i)-step, t_lost_signal(i)+step, 'FaceColor', darK_yellow);
@@ -58,11 +60,11 @@ for i = 1: length(t_lost_signal)
         xregion(sp(3), t_lost_signal(i)-step, t_lost_signal(i)+step, 'FaceColor', darK_yellow);
 end
 
-
+legend("Sinal", "rain");
 %% Temperature influence plots
 
 temp_signal_firgure = figure;
-plot(sinal.Date, sinal.Val/max(sinal.Val) -1 , '.-', temp_and_rain.datetime, temp_and_rain.temp, '.-'); title('Influence of temperature in signal'); grid on;
+plot(sinal.Date, -ruido.Val/max(ruido.Val) , '.-', temp_and_rain.datetime, temp_and_rain.temp/max(temp_and_rain.temp), '.-'); title('Influence of temperature in signal'); grid on;
 
 
 
